df67c6f38f03571bb0240a1e9ebdc633fdd091a9
Mil0w0 <lhilderal@myges.fr>
2025-04-22 16:24:39 +0200

    adding basic browser router

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,7 @@
 import './App.css'
 import 'react'
-import { LogIn } from '@/components/pages/LogIn.tsx'
-
 function App() {
-    return (
-        <>
-            <div className="main h-svh w-svw p-2 flex justify-center items-center">
-                <LogIn />
-            </div>
-        </>
-    )
+    return <></>
 }
 
 export default App
