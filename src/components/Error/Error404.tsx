6ec7e8ab84fc110325da14dcd366ffb16c23f295
Loriane Hilderal <loriane.hilderal@canal-plus.com>
2025-05-01 00:15:18 +0200

    init studentBatches

---
+++ src/components/Error/Error404.tsx
@@ -0,0 +1,7 @@
+export function Error404() {
+    return (
+        <div className="main h-svh w-svw p-2 flex justify-center items-center">
+            Erreur 404 !
+        </div>
+    )
+}
---
+++ src/components/ManageStudentBatches/index.tsx
@@ -0,0 +1,72 @@
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from '@/components/ui/table'
+import { Badge } from '@/components/ui/badge.tsx'
+import { StudentBatches } from '@/components/ManageStudentBatches/types.ts'
+export default function StudentBatchesPage() {
+    const studentBatches: StudentBatches = [
+        {
+            id: 'ziohf28d',
+            state: 'Active',
+            name: 'Classe AL1',
+            students: 42,
+            createdAt: new Date(),
+            tags: 'ESGI',
+        },
+        {
+            id: 'ziohf29d',
+            state: 'Inactive',
+            name: 'Classe AL2',
+            students: 25,
+            createdAt: new Date(),
+            tags: 'ESGI',
+        },
+    ]
+    return (
+        <>
+            {/*<h2>My student batches</h2>*/}
+            {/*<PampButton />*/}
+            <Table>
+                <TableHeader>
+                    <TableRow>
+                        <TableHead className="w-[100px]">State</TableHead>
+                        <TableHead>Name</TableHead>
+                        <TableHead>Students</TableHead>
+                        <TableHead>Created at</TableHead>
+                        <TableHead>Tags</TableHead>
+                        <TableHead>Actions</TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    {studentBatches.map((batch) => (
+                        <TableRow key={batch.id}>
+                            <TableCell className="font-medium">
+                                <Badge
+                                    variant={
+                                        batch.state.toUpperCase() === 'ACTIVE'
+                                            ? 'default'
+                                            : 'secondary'
+                                    }
+                                >
+                                    {batch.state}
+                                </Badge>
+                            </TableCell>
+                            <TableCell>{batch.name}</TableCell>
+                            <TableCell>{batch.students}</TableCell>
+                            <TableCell>
+                                {batch.createdAt.toDateString()}
+                            </TableCell>
+                            <TableCell>{batch.tags}</TableCell>
+                            <TableCell>...</TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </>
+    )
+}
