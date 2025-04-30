6d0bf4b38ea0d5ee2635184a7d829e266614db29
Loriane Hilderal <loriane.hilderal@canal-plus.com>
2025-04-30 22:32:44 +0200

    teacher register and error handling

--- src/components/Register/TeacherRegister.tsx
+++ src/components/Register/TeacherRegister.tsx
@@ -3,16 +3,50 @@ import { GalleryVerticalEnd } from 'lucide-react'
 import { Label } from '@/components/ui/label.tsx'
 import { Input } from '@/components/ui/input.tsx'
 import { Button } from '@/components/ui/button.tsx'
-import React from 'react'
+import React, { ChangeEvent, FormEvent, useState } from 'react'
+import { TeacherRegisterDto } from '@/components/Register/types.ts'
+import { authService } from '@/services/auth-api-client.ts'
+import { toast } from 'sonner'
 
 function TeacherRegister({ className, ...props }: React.ComponentProps<'div'>) {
+    const [formData, setFormData] = useState<TeacherRegisterDto>({
+        email: '',
+        password: '',
+        first_name: '',
+        last_name: '',
+    })
+
+    const [isLoading, setIsLoading] = useState(false)
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const { id, value } = e.currentTarget
+        setFormData((prev) => ({ ...prev, [id]: value }))
+    }
+
+    const handleSubmit = async (e: FormEvent) => {
+        e.preventDefault()
+        setIsLoading(true)
+        try {
+            const response = await authService.register(formData)
+            if (response.success) {
+                window.location.href = '/login'
+            } else {
+                toast.error(response.error)
+            }
+        } catch (error) {
+            toast.error('Une erreur est survenue.')
+            console.error(error)
+        } finally {
+            setIsLoading(false)
+        }
+    }
+
     return (
         <div className={cn('flex flex-col w-1/4 gap-6', className)} {...props}>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="flex flex-col gap-6">
                     <div className="flex flex-col items-center gap-2">
                         <a
-                            href="/"
+                            href="/public"
                             className="flex flex-col items-center gap-2 font-medium"
                         >
                             <div className="flex size-8 items-center justify-center rounded-md">
@@ -41,24 +75,27 @@ function TeacherRegister({ className, ...props }: React.ComponentProps<'div'>) {
                                 type="email"
                                 placeholder="m@example.com"
                                 required
+                                onChange={handleChange}
                             />
                         </div>
                         <div className="grid gap-3">
-                            <Label htmlFor="surname">Surname</Label>
+                            <Label htmlFor="last_name">Last name</Label>
                             <Input
-                                id="surname"
+                                id="last_name"
                                 type="text"
                                 placeholder="Ex: Hernandez"
                                 required
+                                onChange={handleChange}
                             />
                         </div>
                         <div className="grid gap-3">
                             <Label htmlFor="firstName">First name</Label>
                             <Input
-                                id="firstName"
+                                id="first_name"
                                 type="text"
                                 placeholder="Ex: Paul"
                                 required
+                                onChange={handleChange}
                             />
                         </div>
                         <div className="grid gap-3">
@@ -68,16 +105,22 @@ function TeacherRegister({ className, ...props }: React.ComponentProps<'div'>) {
                                 type="password"
                                 placeholder="********"
                                 required
+                                onChange={handleChange}
                             />
                         </div>
-                        <Button type="submit" className="w-full">
+                        <Button
+                            style={{ cursor: 'pointer' }}
+                            type="submit"
+                            className="w-full"
+                            disabled={isLoading}
+                        >
                             Register
                         </Button>
                     </div>
                 </div>
             </form>
             <div className="text-muted-foreground *:[a]:hover:text-primary text-center text-xs text-balance *:[a]:underline *:[a]:underline-offset-4">
-                By clicking continue, you agree to our{' '}
+                By clicking register, you agree to our{' '}
                 <a href="/termsOfServices">Terms of Service</a> and{' '}
                 <a href="/privacyPolicies">Privacy Policy</a>.
             </div>
--- src/components/layout/CenteredLayout.tsx
+++ src/components/layout/CenteredLayout.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router'
+import { Toaster } from 'sonner'
 
 export default function CenteredLayout() {
     return (
         <div className="main h-svh w-svw p-2 flex justify-center items-center">
             <Outlet />
+            <Toaster richColors position={'top-right'} />
         </div>
     )
 }
