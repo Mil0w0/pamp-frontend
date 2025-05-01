ac549f088182e2efa19d5587c80cb62daad6107a
Loriane Hilderal <loriane.hilderal@canal-plus.com>
2025-05-01 17:25:31 +0200

    adding studentBatches table and preparing interactions

--- src/components/ManageStudentBatches/index.tsx
+++ src/components/ManageStudentBatches/index.tsx
@@ -6,9 +6,22 @@ import {
     TableHeader,
     TableRow,
 } from '@/components/ui/table'
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
 import { Badge } from '@/components/ui/badge.tsx'
 import { StudentBatches } from '@/components/ManageStudentBatches/types.ts'
+import PampButton from '@/components/ui/pamp-button.tsx'
+import { toast } from 'sonner'
+import { Button } from '@/components/ui/button.tsx'
+import { useNavigate } from 'react-router'
 export default function StudentBatchesPage() {
+    const navigate = useNavigate()
     const studentBatches: StudentBatches = [
         {
             id: 'ziohf28d',
@@ -27,10 +40,22 @@ export default function StudentBatchesPage() {
             tags: 'ESGI',
         },
     ]
+
+    function goToStudentBatchesById(id: string) {
+        navigate(`/student-batches/${id}`)
+    }
+
     return (
-        <>
-            <h1>My student batches</h1>
-            {/*<PampButton />*/}
+        <div className="p-24 flex flex-col gap-8">
+            <div className="flex justify-between">
+                <h1 className="text-2xl font-semibold">My student batches</h1>
+                <PampButton
+                    message={'New batch'}
+                    onClick={() => {
+                        toast('New batch not implemented yet')
+                    }}
+                />
+            </div>
             <Table>
                 <TableHeader>
                     <TableRow>
@@ -44,7 +69,7 @@ export default function StudentBatchesPage() {
                 </TableHeader>
                 <TableBody>
                     {studentBatches.map((batch) => (
-                        <TableRow key={batch.id}>
+                        <TableRow key={batch.id} className="h-4">
                             <TableCell className="font-medium">
                                 <Badge
                                     variant={
@@ -62,11 +87,41 @@ export default function StudentBatchesPage() {
                                 {batch.createdAt.toDateString()}
                             </TableCell>
                             <TableCell>{batch.tags}</TableCell>
-                            <TableCell>...</TableCell>
+                            <TableCell>
+                                <DropdownMenu>
+                                    <DropdownMenuTrigger asChild>
+                                        <Button
+                                            style={{ cursor: 'pointer' }}
+                                            variant="outline"
+                                        >
+                                            ...
+                                        </Button>
+                                    </DropdownMenuTrigger>
+                                    <DropdownMenuContent>
+                                        <DropdownMenuLabel>
+                                            I want to ...{' '}
+                                        </DropdownMenuLabel>
+                                        <DropdownMenuSeparator />
+                                        <DropdownMenuItem
+                                            style={{ cursor: 'pointer' }}
+                                        >
+                                            Delete this
+                                        </DropdownMenuItem>
+                                        <DropdownMenuItem
+                                            style={{ cursor: 'pointer' }}
+                                            onClick={() =>
+                                                goToStudentBatchesById(batch.id)
+                                            }
+                                        >
+                                            Modify this
+                                        </DropdownMenuItem>
+                                    </DropdownMenuContent>
+                                </DropdownMenu>
+                            </TableCell>
                         </TableRow>
                     ))}
                 </TableBody>
             </Table>
-        </>
+        </div>
     )
 }
