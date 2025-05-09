4d7c1a710127726f755736db792169a807e995c5
Loriane Hilderal <loriane.hilderal@canal-plus.com>
2025-05-09 16:20:21 +0200

    adding dark mode and finished mobile header

--- src/main.tsx
+++ src/main.tsx
@@ -10,20 +10,32 @@ import StudentBatchesPage from '@/components/ManageStudentBatches'
 import { Error404 } from '@/components/Error/Error404.tsx'
 import { Toaster } from 'sonner'
 import StudentBatchById from '@/components/ManageStudentBatches/StudentBatchById.tsx'
+import { ThemeProvider } from '@/components/ui/theme-provider'
 
 createRoot(document.getElementById('root')!).render(
-    <BrowserRouter>
-        <CustomHeader />
-        <Routes>
-            <Route element={<CenteredLayout />}>
-                <Route path="/login" element={<LogIn />} />
-                <Route path="/register/teacher" element={<TeacherRegister />} />
-            </Route>
-            <Route path="/student-batches/" element={<StudentBatchesPage />} />
-            <Route path="/student-batches/:id" element={<StudentBatchById />} />
-            <Route path="/" element={<App />} />
-            <Route path="*" element={<Error404 />} />
-        </Routes>
-        <Toaster richColors position={'top-right'} />
-    </BrowserRouter>
+    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+        <BrowserRouter>
+            <CustomHeader />
+            <Routes>
+                <Route element={<CenteredLayout />}>
+                    <Route path="/login" element={<LogIn />} />
+                    <Route
+                        path="/register/teacher"
+                        element={<TeacherRegister />}
+                    />
+                </Route>
+                <Route
+                    path="/student-batches/"
+                    element={<StudentBatchesPage />}
+                />
+                <Route
+                    path="/student-batches/:id"
+                    element={<StudentBatchById />}
+                />
+                <Route path="/" element={<App />} />
+                <Route path="*" element={<Error404 />} />
+            </Routes>
+            <Toaster richColors position={'top-right'} />
+        </BrowserRouter>
+    </ThemeProvider>
 )
