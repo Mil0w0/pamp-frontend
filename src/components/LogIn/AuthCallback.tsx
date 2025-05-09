86fba78ea95083826dd506230aa2251c712919f2
Loriane Hilderal <loriane.hilderal@canal-plus.com>
2025-05-09 23:18:45 +0200

    adding sso login, handling token

---
+++ src/components/LogIn/AuthCallback.tsx
@@ -0,0 +1,16 @@
+import { useNavigate, useSearchParams } from 'react-router'
+import { toast } from 'sonner'
+
+export default function AuthCallback() {
+    const [params, setSearchParams] = useSearchParams()
+    const navigate = useNavigate()
+    const token = params.get('token') || 'unknown'
+    console.log(params)
+    if (token === 'unknown') {
+        return 'pas de token'
+    } else {
+        localStorage.setItem('token', token) //might be a sensible issue
+        toast.success('Successfully logged in via SSO')
+        navigate('/')
+    }
+}
