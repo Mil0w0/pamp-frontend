a8cb2a8df12535dc19f9a20cd6066d9879a611b5
Loriane Hilderal <loriane.hilderal@canal-plus.com>
2025-06-20 22:04:25 +0200

    protecting routes and global currentUser available

--- src/components/LogIn/Logout.tsx
+++ src/components/LogIn/Logout.tsx
@@ -1,15 +1,23 @@
 import { toast } from 'sonner'
 import { useNavigate } from 'react-router'
 import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { clearCurrentUser } from '@/store/user.slice.ts'
 
 export default function Logout() {
     const navigate = useNavigate()
+    const dispatch = useDispatch()
 
     useEffect(() => {
-        localStorage.removeItem('auth_token')
-        toast.success('You have logged out')
-        navigate('/')
-    }, [navigate])
+        const redirectTimeout = setTimeout(() => {
+            localStorage.removeItem('auth_token')
+            dispatch(clearCurrentUser())
+            toast.success('You have logged out')
+            navigate('/')
+        }, 200) // wait 200ms before evaluating redirect conditions
+
+        return () => clearTimeout(redirectTimeout)
+    }, [navigate, dispatch])
 
     return null
 }
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import projectReducer from './project.slice.ts'
+import userReducer from './user.slice.ts'
 
 export const store = configureStore({
     reducer: {
         project: projectReducer,
+        user: userReducer,
     },
 })
 
