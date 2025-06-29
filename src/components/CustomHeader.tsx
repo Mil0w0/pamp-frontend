b8ce843fb70932fffd0b76719c769f762e56d2c2
Clarence HIRSCH <chirsch1@myges.fr>
2025-06-29 16:37:08 +0200

    final ui adjustments

--- src/components/CustomHeader.tsx
+++ src/components/CustomHeader.tsx
@@ -25,7 +25,7 @@ function CustomHeader() {
     }, [localStorage.getItem('auth_token')])
 
     return (
-        <header className="bg-sidebar-primary text-sidebar-primary-foreground fixed top-0 left-0 right-0 z-50">
+        <header className="bg-sidebar-primary text-sidebar-primary-foreground sticky top-0 left-0 right-0 z-50">
             <nav
                 className="mx-auto flex max-w-7xl items-center justify-right p-6 lg:px-8 border-b-1 border-secondary"
                 aria-label="Global"
